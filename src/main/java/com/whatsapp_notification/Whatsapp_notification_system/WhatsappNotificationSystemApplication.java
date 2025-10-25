6c324b44268a15f16c651fa075b2ab0b8ea36f46
Divyansh Kumar <kumardivyansh62@gmail.com>
2025-10-25 23:57:24 +0530

    First commit

---
+++ src/main/java/com/whatsapp_notification/Whatsapp_notification_system/WhatsappNotificationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.whatsapp_notification.Whatsapp_notification_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappNotificationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappNotificationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whatsapp_notification/Whatsapp_notification_system/WhatsappNotificationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whatsapp_notification.Whatsapp_notification_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappNotificationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
